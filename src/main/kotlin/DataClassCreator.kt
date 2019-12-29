55fcd737b8401a7981e1a8d0c3661666a35761b6
adam2809 <adam.kule@gmail.com>
2019-12-29 20:45:34 +0100

    Basic version works

--- src/main/kotlin/DataClassCreator.kt
+++ src/main/kotlin/DataClassCreator.kt
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.io.File
 import java.lang.StringBuilder
 
-class DataClassCreator(val sourceFile: File){
+class DataClassCreator(private val root:JsonNode, private val topLevelClassName: String){
+    constructor(sourceFile:File,topLevelClassName: String):this(jacksonObjectMapper().readTree(sourceFile),topLevelClassName)
 
-    val dataClassBuilder = StringBuilder()
+    val dataClassList = mutableListOf<DataClass>()
 
-    val jsonTypeToActionMapping = mapOf<JsonNodeType,(JsonNode) -> String>(
-        JsonNodeType.ARRAY to { str:JsonNode -> "String" },
-        JsonNodeType.BOOLEAN to {bool:JsonNode -> "Boolean" },
+    val jsonTypeToActionMapping = mapOf(
+        JsonNodeType.STRING to ::resolveString,
+        JsonNodeType.BOOLEAN to ::resolveBool,
         JsonNodeType.NUMBER to ::resolveIntOrDouble,
         JsonNodeType.ARRAY to ::resolveArrayType,
         JsonNodeType.OBJECT to ::addObjectJsonNode
@@ -21,23 +22,66 @@ class DataClassCreator(val sourceFile: File){
     }
 
     private fun makeDataClass() {
-        val mapper = jacksonObjectMapper()
-        val root: JsonNode = mapper.readTree(sourceFile)
-
-
+        addObjectJsonNode(mapOf(topLevelClassName to root).entries.first(),-1)
     }
 
-    private fun addObjectJsonNode(node:JsonNode):String{
-        node.fields().forEach {
+    private fun addObjectJsonNode(node:Map.Entry<String,JsonNode>,index:Int){
+        dataClassList.add(DataClass(node.key, mutableListOf()))
+        val currLastIndex = dataClassList.lastIndex
 
+        if(index >= 0){
+            addPropertyToDataClass(index,"DataClass$currLastIndex",node.key)
+        }
+
+        node.value.fields().forEach {
+            val action = jsonTypeToActionMapping[it.value.nodeType]
+            action?.invoke(it,currLastIndex)
         }
     }
 
-    private fun resolveIntOrDouble(num:JsonNode):String{
+    private fun resolveIntOrDouble(num:Map.Entry<String,JsonNode>,index:Int){
+        val numStr = num.value.toPrettyString()
+
+        val numType = if (numStr.contains(".")) "Double" else "Int"
+        addPropertyToDataClass(index,numType,num.key)
+    }
+
+    private fun resolveArrayType(arr:Map.Entry<String,JsonNode>,index:Int){
 
     }
 
-    private fun resolveArrayType(arr:JsonNode):String{
+    private fun resolveBool(bool:Map.Entry<String,JsonNode>,index:Int){
+        addPropertyToDataClass(index,"Boolean",bool.key)
+    }
 
+    private fun resolveString(str:Map.Entry<String,JsonNode>,index:Int){
+        addPropertyToDataClass(index,"String",str.key)
+    }
+
+    private fun addPropertyToDataClass(index:Int,type:String,name:String){
+        dataClassList[index].properties.add(DataClassProperty(type,name))
+    }
+
+    override fun toString(): String {
+        val builder = StringBuilder()
+        dataClassList.forEachIndexed { i, dataClass ->
+            builder.append("data class ${if (i==0) topLevelClassName else "DataClass$i"}(\n")
+            dataClass.properties.forEach {
+                builder.append("\tval ${it.name}:${it.type},\n")
+            }
+            builder.setLength(builder.length-2)
+            builder.append("\n)\n\n")
+        }
+        return builder.toString()
     }
 }
+
+data class DataClass(
+    val name:String,
+    var properties:MutableList<DataClassProperty>
+)
+
+data class DataClassProperty(
+    val type:String,
+    val name:String
+)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
 
 fun main(args:Array<String>){
-    val creator = DataClassCreator(File("/home/adam/code/JsonTypeInference/src/main/resources/test.json"))
+    val creator = DataClassCreator(File("/home/adam/code/JsonTypeInference/src/main/resources/test.json"),"Test")
     println(creator)
 }
