d5f0d8e44a11d96d57a3f9d6fa373418b95f4a81
adam2809 <adam.kule@gmail.com>
2019-12-31 17:07:16 +0100

    Minor refacor of Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,8 @@
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
-import java.io.PrintWriter
 
-fun main(args:Array<String>){
+fun main(){
     val config = jacksonObjectMapper().readValue<List<Config>>(File("${System.getProperty("user.dir")}/src/main/resources/config.json"))
     config.forEach {
         val creator = DataClassCreator(File(it.source),it.className)
